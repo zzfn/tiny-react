73dabaa2ecc1613a7ac55c964d0e6bb543c02684
chenc <chenchen3@fosun.com>
2021-05-21 18:25:22 +0800

    feat: 简易react

--- examples/index.js
+++ examples/index.js
@@ -1,3 +1,35 @@
-const json={"nodeName":"div","props":{},"children":[{"nodeName":"h1","props":{"className":"a1","onClick":"testclick"},"children":[{"nodeName":"span","props":{"className":"b1"},"children":["123321"]},{"nodeName":"span","props":{"className":"b2"},"children":["abccba"]}]},{"nodeName":"h2","props":{"className":"a2"},"children":["456654"]}]}
-React.render(json,document.getElementById('root'));
-console.log(React)
+function sleep(delay) {
+    let current=Date.now()
+    while (Date.now()-current<delay){}
+    // for(;Date.now()-current<delay;){}
+}
+const walklist=[
+    ()=>{
+        console.log('1-start');
+        sleep(20);
+        console.log('1-end')
+    },
+    ()=>{
+        console.log('2-start');
+        sleep(30);
+        console.log('2-end')
+    },
+    ()=>{
+        console.log('3-start');
+        sleep(30);
+        console.log('3-end')
+    }
+]
+function walk(deadline) {
+    console.log(`剩余${deadline}`)
+    console.log(deadline.didTimeout)
+    while (deadline.timeRemaining()){
+    }
+    if(walklist.length){
+        requestAnimationFrame(walk)
+    }
+}
+function getTask(){
+    walklist.shift()()
+}
+requestAnimationFrame(walk)
--- src/react/render.js
+++ src/react/render.js
@@ -1,6 +1,7 @@
 import {_render} from "./_render";
 
 function render(vNode, container) {
+    container.innerHTML=''
     return container.appendChild(_render( vNode ));    // 将渲染结果挂载到真正的DOM上
 }
 
